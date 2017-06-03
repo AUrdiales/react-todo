4fb6bf432afd3440ea88e1490c5f98ce4a071f41
AUrdiales <alejandro.urdiales.93@gmail.com>
2017-06-03 16:56:07 +0200

    dates added

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,15 +1,30 @@
 import React, {Component} from 'react';
+import moment from 'moment';
 
 class Todo extends Component {
     
     render() {
         
-        var {id,text, completed} = this.props;
+        var {id,text, completed, createdAt, completedAt} = this.props;
+        var renderDate = () => {
+            
+          
+            if(completed) {
+            var message = 'completed: ';
+            var timestamp = completedAt;
+             return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
+            } else if(!completed) {
+            var message = 'created: ';
+            var timestamp = createdAt;
+            return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm: a');
+            }
+        }
         return (
             <div onClick={() => {this.props.onToggle(id)}} >
                 <label>
                 <input type="checkbox" defaultChecked={completed}/>
-                 {text}
+                 <p>{text}</p>
+                  <p>{renderDate()}</p>
                  </label>
             </div>
         )
