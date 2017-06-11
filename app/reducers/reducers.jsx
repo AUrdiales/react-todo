ba26909477b1dea656b1d206e107e3708d8af167
AUrdiales <alejandro.urdiales.93@gmail.com>
2017-06-11 15:32:45 +0200

    add todoReducer

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,10 +1,14 @@
+import uuid from 'uuid';
+import moment from 'moment';
+
+
 export function searchTextReducer(state = '', action) {
 
         switch(action.type) {
             case 'SET_TEXT_SEARCH':
             return action.searchText;
             default:
-            return state
+            return state;
         };
 };
 
@@ -12,8 +16,40 @@ export function showCompletedReducer(state = false, action){
 
     switch(action.type) {
         case 'TOGGLE_SHOW_COMPLETED':
-        return !state
+        return !state;
         default:
-        return state
+        return state;
+    };
+};
+
+export function todosReducer(state = [], action) {
+    
+    switch(action.type) {
+        case 'ADD_TODO':
+        return [
+            ...state,
+            {
+                id: uuid(),
+                text: action.text,
+                createdAt: moment().unix(),
+                completedAt: undefined
+            }
+            
+        ];
+        case 'TOGGLE_TODO':
+        return state.map((todo) => {
+            if(todo.id === action.id){
+                var nextCompleted = !todo.completed;
+                var nextCompletedAt = nextCompleted ? moment().unix() : undefined;
+                return {
+                    ...todo,
+                    completed: nextCompleted,
+                    completedAt = nextCompletedAt
+                }
+            }
+        });
+        
+        default:
+        return state;
     };
 };
